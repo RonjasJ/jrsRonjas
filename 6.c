984ccf154a57c743d44449770e85249b1fc08b24
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-01-18 16:21:54 +0530

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,16 @@
+#include<stdio.h.
+int main()
+{
+int a,b,c;
+printf("enter numbers");
+scanf("%d%d%d",a,b,c);
+if((a>b)&&(a>c))
+{
+elseif(b>c)
+printf("big %d"c);
+else
+printf("big %d",b);
+else
+printf("big %d",a);
+}
+}
