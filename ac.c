37e4fd5f157724def8ce0718f558e65bc5f59fbb
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-01-19 16:35:47 +0530

    Create ac.c

---
+++ ac.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+int num,m,n; 
+printf("Print Odd Numbers in a given range m to n:\n");
+scanf("%d%d",&m,&n);
+for (num = m; num <= n; num++)
+{
+if (num % 2 == 0)
+printf ("%d ", num);
+}
+}
