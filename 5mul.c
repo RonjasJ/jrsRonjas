40cb7660138b49ad5f7b53b6e5d5a428f7d1a8ba
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-01-21 09:36:17 +0530

    Create 5mul.c

---
+++ 5mul.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+        int i,n,k;
+        printf("\n Please Give The Value of N:  ");
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {   k=5*i;
+            printf("\n%d",k);
+        }
+        
+}
