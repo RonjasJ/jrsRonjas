d538fec0755012e1862d7fa37ec6d1c5d6c8bf2c
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-01-21 11:06:07 +0530

    Create e.c

---
+++ e.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void sort_numbers_ascending(int number[], int count)
+{
+   int temp, i, j, k;
+   for (j = 0; j < count; ++j)
+   {
+      for (k = j + 1; k < count; ++k)
+      {
+         if (number[j] > number[k])
+         {
+            temp = number[j];
+            number[j] = number[k];
+            number[k] = temp;
+         }
+      }
+   }
+   printf("Numbers in ascending order:\n");
+   for (i = 0; i < count; ++i)
+      printf("%d\n", number[i]);
+}
+void main()
+{
+   int i, count, number[20]={7,3,1,8,9,4,2}; 
+   printf("How many numbers you are gonna enter:");
+   scanf("%d", &count);  
+   for (i = 0; i < count; ++i) 
+   sort_numbers_ascending(number, count);
+}
