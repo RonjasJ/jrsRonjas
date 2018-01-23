98684bdc9274bd1a8d3e56ab237464fee625eda7
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-01-23 16:21:23 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int num,i,n,sumo=0;
+float avg;
+printf("enter the number %d",n);
+scanf("%d",&num);
+for(i=0;i<n;i++)
+{
+sum=sum+num;
+}
+avg=sum/n;
+printf("%f",avg);
+}
