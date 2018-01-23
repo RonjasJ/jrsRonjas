c42a578191d5d16c23c7c4f1364d12000f821eb4
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-01-23 15:58:49 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+char s[20];
+printf("entern value");
+scanf("%d",&n);
+printf("enter string");
+scanf("%s",&s)
+for(i=0;i<n;i++)
+{
+printf("%s/n",s);
+}
+}
