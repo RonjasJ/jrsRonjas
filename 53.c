40c15a4e2b4826bcaf24aca5db978f3076b0fd8c
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-01-23 16:46:31 +0530

    Update 53.c

--- 53.c
+++ 53.c
@@ -4,10 +4,11 @@ int main()
   int num, i=0, sum=0, d;
   printf ("Enter an integer value: \n");
   scanf ("%d", &num);
-  while (num)
+  while (num!=0)
   {
     d = num %10;
     sum=sum+d;
     num = num /10;
     }
-    printf("")
+    printf("%d",sum);
+}
