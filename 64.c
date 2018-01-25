cdc9adde60b434093a3d574cc331c1e992d22635
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-01-25 16:27:09 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main() 
+{
+	int a,b,n;
+	printf("\nEnter the two numbers: ");
+	scanf("%d",&a);
+	scanf("%d",&b);
+		n=a+b;
+	if(n%2==0)
+	{
+		printf("\nThe sum is even.");
+	}
+	else
+	{
+		printf("\nThe sum is odd. ");
+	}
+}
