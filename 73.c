d6effd470cddcb5e91f9da9b1f1d681a6f1be5d4
RonjasJ <30828377+RonjasJ@users.noreply.github.com>
2018-02-01 16:02:14 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int l,i,m,n,count=0;
+	scanf("%d",&l);
+	scanf("%d%d",&m,&n);
+	for(i=m;i<=n;i++)
+	{
+		if(l==i)
+		{
+			count++;
+		}
+	}
+	if(count==1)
+	 printf("yes");
+	else
+	 printf("No"):
+}
